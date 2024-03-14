fdf1b0da01137f165e447cc7bb91fff3eecbc44a
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-15 01:20:15 +0300

    Refactored lockboxes solution to remove dependency on collections module

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,32 +2,23 @@
 """Lockboxes challenge"""
 
 
-from collections import deque
 def canUnlockAll(boxes):
-    """
-    Checks if all boxes in the given list of boxes can be unlocked.
-    Parameters:
-	- boxes (list): A list of lists representing the boxes. Each inner list contains the keys that can unlock the corresponding box.
+    '''
+    Determines if all the boxes can be opened or not.
 
-	Returns:
-	- bool: True if all boxes can be unlocked, False otherwise.
+    Args:
+        boxes (list): A list of lists representing boxes and their keys.
 
-	Example:
-	>>> boxes = [[1], [2], [3], []]
-	>>> canUnlockAll(boxes)
-	True
-
-	>>> boxes = [[1, 2], [3], [], []]
-	>>> canUnlockAll(boxes)
-	False
-	"""
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    '''
     num_boxes = len(boxes)
     visited = set()  # Set to track visited boxes
-    queue = deque([0])  # Start with box 0
+    queue = [0]  # Start with box 0
     visited.add(0)  # Mark box 0 as visited
 
     while queue:
-        current_box = queue.popleft()  # Get the next box to explore
+        current_box = queue.pop(0)  # Get the next box to explore
         for key in boxes[current_box]:
             if key < num_boxes and key not in visited:
                 visited.add(key)  # Mark the box as visited
