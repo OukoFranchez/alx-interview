227ae412999a4e2259e8d556c3fff58b25c76eed
OukoFranchez <oukofranchez@gmail.com>
2024-03-21 07:14:26 +0300

    refactor the task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,35 +2,35 @@
 """ Minimum Operations Task Module """
 
 
+from math import sqrt
+
+
 def minOperations(n):
     """
-Calculate the minimum number of operations required to obtain a
-string of length 'n' using the following operations:
-- Append the current string with its reverse
-- Append the current string with a copy of itself
-
-Parameters:
-n (int): The length of the desired string
-
-Returns:
-int: The minimum number of operations required to obtain the string
-
-Example:
->>> minOperations(5)
-3
-"""
+        Function that computes the minimum operations based on
+        the requirements specified above
+    """
     if n <= 1:
         return 0
 
-    """
-    Initializing an array to store minimum operations
-    for each number of characters
-    """
-    min_ops = [0] * (n + 1)
+    value = findPrimeFactor(n)
+    temp = n
+    result = 0
+    while value != temp:
+        result += value
+        temp /= value
+        value = findPrimeFactor(temp)
 
-    for i in range(2, n + 1):
-        min_ops[i] = float('inf')  # Set initial minimum to infinity
-        for j in range(1, i):
-            if i % j == 0:
-                min_ops[i] = min(min_ops[i], min_ops[j] + i // j)
-    return min_ops[n]
+    result += value
+    return int(result)
+
+
+def findPrimeFactor(n):
+    """
+        Function that finds the lowest prime factor of a number,
+        if the number is prime, returns the number itself
+    """
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return i
+    return n
